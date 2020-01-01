ae0f85caa22af0e592d9a04ae3579b3e43a58261
ma7veev <ma7veev@gmail.com>
2020-01-01 12:48:45 +0200

    add css and categories module

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categories;
+use App\Form\CategoriesType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoriesController extends AbstractController
+{
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function index()
+    {
+        return $this->render('categories/index.html.twig', [
+            'controller_name' => 'CategoriesController',
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/categories/create", name="categories_create")
+     */
+    public function create(Request $request)
+    {
+
+        $categories = new Categories;
+        $form = $this->createForm(CategoriesType::class, $categories);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+        }
+        return $this->render('categories/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Entity/Categories.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoriesRepository")
+ */
+class Categories
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $parent;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $sort;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getParent(): ?int
+    {
+        return $this->parent;
+    }
+
+    public function setParent(int $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getSort(): ?int
+    {
+        return $this->sort;
+    }
+
+    public function setSort(?int $sort): self
+    {
+        $this->sort = $sort;
+
+        return $this;
+    }
+}
